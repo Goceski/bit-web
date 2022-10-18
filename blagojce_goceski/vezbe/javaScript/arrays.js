b87a256cec689a908001b85f9fec081e849bf556
Goceski <goceski@gmail.com>
2022-10-18 23:55:27 +0200

    arrays and loops

---
+++ blagojce_goceski/vezbe/javaScript/arrays.js
@@ -0,0 +1,24 @@
+// Vezba 1
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+console.log(months.length);
+console.log(months[0] + " " + months[5] + " " + months[9]);
+
+// Vezba 2
+var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sathurday", "Sunday"];
+console.log(days.length);
+console.log(days[6]);
+
+// Vezba 3
+var negatives = [2, -4, 5, -2, -11];
+console.log(negatives[1], negatives[3], negatives[4]);
+
+// Vezba 4
+var b = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+console.log(b[1], b[4], b[5], b[6], b[8]);
+
+// Vezba 5
+var c = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+console.log(c);
+console.log(c[0][3]);
+console.log(c[2]);
+console.log(c[2][1]);
---
+++ blagojce_goceski/vezbe/javaScript/loops.js
@@ -0,0 +1,46 @@
+// Vezba 3 so Loops
+var negatives = [2, -4, 5, -2, -11];
+
+// while
+console.log("Petlja 'while'");
+var i = 0;
+while (i < negatives.length){
+    var element = negatives[i];
+    if(element < 0){
+        console.log("Negativan broj:", element);
+    }
+    i++;
+}
+
+// do-while
+console.log("");
+console.log("Petlja 'do-while'");
+var y = 0;
+do {
+    var br = negatives[y];
+    if (br < 0){
+        console.log("Negativan broj:", br);
+    }     
+    y++;
+}   while (y < negatives.length);
+
+
+// for
+console.log("");
+console.log("Petlja 'for'");
+for (var i = 0; i < negatives.length; i++){
+    var elem = negatives[i];
+    if(elem < 0){
+        console.log("Negativan broj:", elem);
+    }
+}
+
+// for-in
+console.log("");
+console.log("Petlja 'for-in'");
+for(var key in negatives){     // key e pozicija na elementot 0 1 2 3 4
+    var broj = negatives[key];
+    if(broj < 0){
+        console.log("Negativan broj:", broj);
+    }
+}
