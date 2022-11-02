f9d7d33510498d9a2f32ac9534474061ba5473d8
Goceski <goceski@gmail.com>
2022-11-02 09:46:17 +0100

    02.11.2022.

--- blagojce_goceski/vezbe/javaScript/functions5_011122.js
+++ blagojce_goceski/vezbe/javaScript/functions5_011122.js
@@ -225,11 +225,46 @@ isPrime(20);
 // Exercise 11 - Check if a given string is a palindrome (spaces are ignored).
 // Input: eye | Geek | a nut for a jar of tuna
 // Output: true | false | true
+function isPalindrome(input) {
+    var a = '';
+    for (i = 0; i < input.length; i++) { // Eliminise prazna mesta
+        if (input[i] != ' ') {
+            a += input[i];
+        }
+    }
+    var b = '';
+    for (j = a.length - 1; j >= 0; j--) { // Okrene string reverse order
+        b += a[j];
+    }
+    a == b ? console.log(true, ': Is palindrome') : console.log(false, ': Is not palindrome');
+}
 
-
-
+isPalindrome('eye');
+isPalindrome('Geek');
+isPalindrome('a nut for a jar of tuna');
 
 
 // Exercise 12 - Write a program that calculates the greatest common divisor of two integers. Note: The greatest common divisor of two non-zero integers is the greatest positive number that divides both numbers with no remainder.
 // Input: 192 42 | 81 9
 // Output: 6     | 9
+function greatestCommonDivisor(a, b) {
+    var manji;
+    var veci;
+    if (a == b) {
+        console.log('The greatest common divisor is', a);
+        return;
+    }
+    a > b ? (manji = b, veci = a) : (manji = a, veci = b);
+    var grComDiv = 1;
+    for (i = 1; i <= manji; i++) {
+        if (veci % i == 0 && manji % i == 0) {
+            grComDiv = i;
+        }
+    }
+    console.log('The greatest common divisor is', grComDiv);
+}
+
+greatestCommonDivisor(192, 42);
+greatestCommonDivisor(9, 81);
+greatestCommonDivisor(20, 5);
+greatestCommonDivisor(1, 19);
