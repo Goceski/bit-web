70e6e120d54065842088b217f8499f2f12e5247e
Goceski <goceski@gmail.com>
2022-11-27 14:49:14 +0100

    SCSS

--- blagojce_goceski/vezbe/javaScript/functions9_sos.js
+++ blagojce_goceski/vezbe/javaScript/functions9_sos.js
@@ -57,6 +57,42 @@ function replacementString(input, str) {
 console.log(replacementString('Programming in JS is super interesting!', 'JS'));
 
 
+// Exercise 2 -Nikola resenje
+function replace(sentence, element, replacement) {
+    if (!sentence || sentence.length == 0 || !element || element.length == 0 || !replacement || replacement.length != 1) {
+        return sentence;
+    }
+
+    var replacementString = '';
+    for (var i = 0; i < element.length; i++) {
+        replacementString += replacement;
+    }
+
+    var result = '';
+    for (var i = 0; i < sentence.length; i++) {
+        var z = i;
+        var word = '';
+        for (var j = 0; j < element.length; j++) {
+            if (sentence[z]) {
+                word += sentence[z]
+                z++;
+            }
+        }
+
+        if (word.toLowerCase() == element.toLowerCase()) {
+            result += replacementString;
+            i += (element.length - 1);
+        } else {
+            result += sentence[i];
+        }
+    }
+
+    return result;
+}
+
+console.log(replace('Programming in JS is super interesting!', 'super', '*'))
+
+
 
 // Exercise 3 - Write a function that inserts a given character on a given position in the string.
 // Input: “Goo morning”, 4, ‘d’
@@ -226,7 +262,7 @@ function sortByAppereance(inputArray) {
 console.log(sortByAppereance(['apple', 'tea', 'amazing', 'morning', 'JavaScript']));
 
 
-// Exercise 8 - ISPRAVNO
+// Exercise 8 - ISPRAVNO Nikola resenje
 function sortArrayByInputLetter(input, letter) {
     if (!input || input.length == 0 || !letter || letter.length != 1) {
         return input;
@@ -246,22 +282,32 @@ function sortArrayByInputLetter(input, letter) {
             var maxCount = 0;
             for (var z = 0; z < result.length; z++) {
                 if (result[z][1] > maxCount) {
-                    maxCount = result[z][1];
+                    maxCount = result[z][1]
                 }
             }
 
-            if (maxCount < counter) {
-                result = [input[i], counter].concat(result);
+            if (maxCount <= counter) {
+                var newElement = [input[i], counter];
+                result = [newElement].concat(result);
+            } else {
+                result.push([input[i], counter])
             }
+
         } else {
-            result.push([input[i], counter]);
+            result.push([input[i], counter])
         }
     }
 
-    return result;
+    var sorted = [];
+    for (var i = 0; i < result.length; i++) {
+        sorted.push(result[i][0])
+    }
+
+    return sorted;
 }
 
-console.log(sortArrayByInputLetter(['apple', 'tea', 'amazing', 'morning', 'JavaScript'], 'a'));
+console.log(sortArrayByInputLetter(['apple', 'tea', 'amazing', 'morning', 'Nostradamus', 'Avala'], 'a'))
+
 
 
 // Exercise 9 - Write a function that prints out the date of the next day.
